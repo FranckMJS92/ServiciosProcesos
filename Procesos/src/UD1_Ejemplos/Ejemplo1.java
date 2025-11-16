07fb73f6f72f1e7c7964faeb94a44e3bda03f9f6
Franck <francklopez92@outlook.es>
2025-11-16 17:23:04 +0100

    First Update

---
+++ Procesos/src/UD1_Ejemplos/Ejemplo1.java
@@ -0,0 +1,45 @@
+package UD1_Ejemplos;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class Ejemplo1 {
+
+    public static void main(String[] args) {
+
+        // /C ejecutamos y cerramos el simbolo del sistema
+        // /K ejecutamos y no se cierra el simbolo del sistema
+        ProcessBuilder pBuilder = new ProcessBuilder("cmd", "/C", "ping", "8.8.8.8");
+        //pBuilder.directory(new File(""));
+        try {
+            Process proceso = pBuilder.start();
+
+            InputStream is = proceso.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+            System.out.println("*** SALIDA ESTÁNDAR ****");
+            String linea = br.readLine();
+            while (linea != null) {
+                System.out.println(linea);
+                linea = br.readLine();
+            }
+
+            InputStream iError = proceso.getErrorStream();
+            BufferedReader brError = new BufferedReader(new InputStreamReader(iError));
+            System.out.println("*** SALIDA ERROR ****");
+            String lineaError = brError.readLine();
+            while (lineaError != null) {
+                System.out.println(lineaError);
+                lineaError = br.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ Procesos/src/UD1_Ejemplos/Suma.java
@@ -0,0 +1,19 @@
+package UD1_Ejemplos;
+
+import java.util.Scanner;
+
+public class Suma {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        System.out.println("La suma es " + (a + b));
+
+        scan.close();
+
+    }
+
+}
