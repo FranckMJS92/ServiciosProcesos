fce52762d4f486c325e045d9953061e233d8eeb9
Franck <francklopez92@outlook.es>
2025-12-30 18:41:40 +0100

    Entregable 2 ejercicio 1

--- Entregable2/src/ejercicio1/Camareros.java
+++ Entregable2/src/ejercicio1/Camareros.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 // Clase que representa a un camarero
 public class Camareros extends Thread {
-    private String nombre;
     private Estanteria estanteria;
     private Random random;
     private int contadorComandas;
@@ -22,17 +21,17 @@ public class Camareros extends Thread {
             while (true) {
                 // Generar una nueva comanda
                 contadorComandas++;
-                String comanda = nombre + " - Comanda " + contadorComandas;
+                String comanda = getName() + " - Comanda " + contadorComandas;
 
                 // Dejar la comanda en la estantería
-                estanteria.dejarComanda(nombre, contadorComandas, comanda);
+                estanteria.dejarComanda(getName(), contadorComandas, comanda);
 
                 // Tiempo aleatorio entre 200 y 400 ms
                 int tiempoEspera = 200 + random.nextInt(201); // 200-400 ms
                 Thread.sleep(tiempoEspera);
             }
         } catch (InterruptedException e) {
-            System.out.println(nombre + " interrumpido.");
+            System.out.println(getName() + " interrumpido.");
             e.printStackTrace();
         }
     }
--- Entregable2/src/ejercicio1/Cocineros.java
+++ Entregable2/src/ejercicio1/Cocineros.java
@@ -4,7 +4,6 @@ import java.util.Random;
 
 // Clase que representa a un cocinero
 public class Cocineros extends Thread {
-    private String nombre;
     private Estanteria estanteria;
     private Random random;
 
@@ -19,17 +18,17 @@ public class Cocineros extends Thread {
         try {
             while (true) {
                 // Recoger una comanda de la estantería
-                String comanda = estanteria.recogerComanda(nombre);
+                String comanda = estanteria.recogerComanda(getName());
 
                 // "Cocinar" la comanda (tiempo aleatorio entre 250 y 350 ms)
                 int tiempoCocina = 250 + random.nextInt(101); // 250-350 ms
                 Thread.sleep(tiempoCocina);
 
                 // Aquí se podría procesar la comanda recibida
-                System.out.println(nombre + " está cocinando: " + comanda);
+                System.out.println(getName() + " está cocinando: " + comanda);
             }
         } catch (InterruptedException e) {
-            System.out.println(nombre + " interrumpido.");
+            System.out.println(getName() + " interrumpido.");
             e.printStackTrace();
         }
     }
