4b8391f701eed2634a97d27c991502215a6133a4
Franck <francklopez92@outlook.es>
2026-01-17 19:39:35 +0100

    Forget Update

---
+++ UD3_Actividad1/src/ejercicio1/Servidor.java
@@ -0,0 +1,73 @@
+package ejercicio1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+
+public class Servidor {
+
+    private static String generarFecha() {
+        return null;
+    }
+
+    private static String generarHoras() {
+        LocalDateTime actual = LocalDateTime.now();
+        int hora = actual.getHour();
+        int minutos = actual.getMinute();
+        int segundos = actual.getSecond();
+
+        String respuesta = String.format("HORA ACTUAL : %02d:%02d:%02d", hora, minutos, segundos);
+        return respuesta;
+    }
+
+    public static void main(String[] args) {
+        try {
+            // Creamos objeto para el servidor socket
+            ServerSocket servidor = new ServerSocket();
+            // Direccion + puerto
+            InetSocketAddress direccion = new InetSocketAddress("localhost", 2000);
+            // Vincular Direccion + puerto al servidor
+            servidor.bind(direccion);
+
+            System.out.println("Servidor creado y escuchando ... ");
+
+            Socket socketCliente = servidor.accept();
+            System.out.println("CLIENTE CONECTADO ... ");
+
+            // Obtenemos los streams del socket para poder enviar y recibir informacion
+            InputStream entrada = socketCliente.getInputStream();
+            OutputStream salida = socketCliente.getOutputStream();
+
+            // Leemos la peticion que nos envia el cliente
+            // Declara una variable donde guardamos mensaje del cliente
+            byte[] mensaje = new byte[100];
+            // Leemos la petifcion que nos envia el cliente y lo guardamos en mensaje
+            entrada.read(mensaje);
+            String texto = new String(mensaje).trim();
+            System.out.println("PETICION RECIBIDA: " + texto);
+
+            // Si es un "1" -> Hora
+
+            // Si es un "2" -> Fecha
+
+            // Enviamos respuesta al cliente
+            String respuesta = "Tamaño: " + texto.length();
+            salida.write(respuesta.getBytes());
+
+            // Cuando terminemos la comunicacioon, cerramos TODO
+            salida.close();
+            entrada.close();
+            socketCliente.close();
+            servidor.close();
+
+        } catch (IOException e) {
+            System.out.println("[ERROR] Al iniciar servidor");
+            e.printStackTrace();
+        }
+
+    }
+}
