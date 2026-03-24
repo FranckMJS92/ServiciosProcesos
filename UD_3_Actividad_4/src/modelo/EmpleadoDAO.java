82ad686eb1c8d2d667026cd0a57f513099f692aa
Franck <francklopez92@outlook.es>
2026-03-24 21:26:00 +0100

    Update Clase Ejemplo POJO

--- UD_3_Actividad_4/src/modelo/EmpleadoDAO.java
+++ UD_3_Actividad_4/src/modelo/EmpleadoDAO.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EmpleadoDAO {
+public class EmpleadoDAO implements IEmpleadoDAO {
 
     // 1. Listar todos los empleados.
     public synchronized List<Empleado> obtenerTodos() {
@@ -36,14 +36,66 @@ public class EmpleadoDAO {
         return listaEmpleados;
     }
 
-    // 2. Buscar un empleado por ID
-    public Empleado buscarPorId(int id) {
+    // 2. Buscar un empleado por ID.
+    public synchronized Empleado buscarPorId(int idEmpleado) {
+
+        try {
+            // Obtenemos la conexion a la BBDD
+            Connection con = DBConnection.getConnection();
+            // Por cada parametro de la consulta ponemos un ?
+            String sql = "SELECT * FROM empleados WHERE id=?";
+            // Creamos consulta preparada
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Reemplazamos cada ? por su valor
+            ps.setInt(1, idEmpleado);
+
+            // Ejecutamos la consulta preparada y nos devuelve un ResulSet
+            // Result set es un cursos o iterados
+            ResultSet rs = ps.executeQuery();
+
+            // Comprobamos si la consulta devuelve un registro
+            if (rs.next()) {
+                // De cada registro obtenemos los campos
+                // Creo un empleado y lo añado al arraylist
+                Empleado e = mapearEmpleado(rs);
+                ;
+                return e;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
-    // 3. Buiiscar empleados por departamento
-    public List<Empleado> buscarPorDepartamento(String departamento) {
-        return null;
+    // 3. Buscar empleados por departamento.
+    public synchronized List<Empleado> buscarPorDepartamento(String departamento) {
+        List<Empleado> listaEmpleados = new ArrayList<Empleado>();
+        try {
+            // Obtenemos la conexion a la BBDD
+            Connection con = DBConnection.getConnection();
+            // Por cada parametro de la consulta ponemos un ?
+            String sql = "SELECT * FROM empleados WHERE departamento=?";
+            // Creamos consulta preparada
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Reemplazamos cada ? por su valor
+            ps.setString(1, departamento);
+
+            // Ejecutamos la consulta preparada y nos devuelve un ResulSet
+            // Result set es un cursos o iterados
+            ResultSet rs = ps.executeQuery();
+
+            // Recorremos todos los registros de la consulta
+            while (rs.next()) {
+                // De cada registro obtenemos los campos
+                Empleado e = mapearEmpleado(rs);
+                listaEmpleados.add(e);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listaEmpleados;
     }
 
     private Empleado mapearEmpleado(ResultSet rs) throws SQLException {
@@ -56,5 +108,57 @@ public class EmpleadoDAO {
         // Creo un empleado y lo añado al arraylist
         Empleado e = new Empleado(id, nombre, apellido, departamento1, puesto, salario);
         return e;
+    }
+
+    @Override
+    public synchronized boolean eliminarEmpleado(int idEmpleado) {
+        try {
+            // Obtenemos la conexion a la BBDD
+            Connection con = DBConnection.getConnection();
+            // Por cada parametro de la consulta ponemos un ?
+            String sql = "DELETE FROM empleados WHERE id=?";
+            // Creamos consulta preparada
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Reemplazamos cada ? por su valor
+            ps.setInt(1, idEmpleado);
+
+            // Ejecutamos la consulta preparada y nos devuelve un entero
+            // que nos indica los registros afectados
+            int filas = ps.executeUpdate();
+
+            return filas > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public synchronized boolean insertaEmpleado(Empleado nuevoEmpleado) {
+        try {
+            // Obtenemos la conexion a la BBDD
+            Connection con = DBConnection.getConnection();
+            // Por cada parametro de la consulta ponemos un ?
+            String sql = "INSERT INTO empleados (nombre, apellido, departamento, puesto, salario) VALUES (?,?,?,?,?)";
+            // Creamos consulta preparada
+            PreparedStatement ps = con.prepareStatement(sql);
+            // Reemplazamos cada ? por su valor
+            ps.setString(1, nuevoEmpleado.getNombre());
+            ps.setString(2, nuevoEmpleado.getApellido());
+            ps.setString(3, nuevoEmpleado.getDepartamento());
+            ps.setString(4, nuevoEmpleado.getPuesto());
+            ps.setDouble(5, nuevoEmpleado.getSalario());
+
+            // Ejecutamos la consulta preparada y nos devuelve un entero
+            // que nos indica los registros afectados
+            int filas = ps.executeUpdate();
+
+            return filas > 0;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- UD_3_Actividad_4/src/modelo/IEmpleadoDAO.java
+++ UD_3_Actividad_4/src/modelo/IEmpleadoDAO.java
@@ -1,5 +1,17 @@
 package modelo;
 
+import java.util.List;
+
 public interface IEmpleadoDAO {
+
+    List<Empleado> obtenerTodos();
+
+    Empleado buscarPorId(int idEmpleado);
+
+    List<Empleado> buscarPorDepartamento(String departamento);
+
+    boolean eliminarEmpleado(int idEmpleado);
+
+    boolean insertaEmpleado(Empleado nuevoEmpleado);
 
 }
